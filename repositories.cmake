ff09f65b85bd0fcd748bdce44e0d49ed5122fe11
Johannes Zarl <johannes@zarl.at>
2014-11-29 15:12:18 +0100

    Move repository URLs into extra file.

---
+++ repositories.cmake
@@ -0,0 +1,14 @@
+## project download locations for each ExternalProject:
+
+set( Boost_DOWNLOAD
+	URL "http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2/download"
+	URL_HASH "MD5=a744cf167b05d72335f27c88115f211d"
+	)
+
+set( OpenSG_DOWNLOAD
+	GIT_REPOSITORY "git://git.code.sf.net/p/opensg/code"
+	)
+
+set( inVRs_DOWNLOAD
+	SVN_REPOSITORY "https://svn.lrz.de/repos/inVRs/branches/inVRs_OSG"
+	)
