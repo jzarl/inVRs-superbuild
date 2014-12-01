ebc4791e7f548146eae15587168dc88d4c9ee845
Johannes Zarl <johannes.zarl@jku.at>
2014-12-01 18:35:36 +0100

    Add OpenSG Support libs.

--- repositories.cmake
+++ repositories.cmake
@@ -1,14 +1,55 @@
 ## project download locations for each ExternalProject:
 
+# version 1.56 is the latest version recognised by cmake 3.0.2:
 set( Boost_DOWNLOAD
 	URL "http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2/download"
 	URL_HASH "MD5=a744cf167b05d72335f27c88115f211d"
+	)
+
+set( ColladaDOM_DOWNLOAD
+	URL "http://downloads.sourceforge.net/project/collada-dom/Collada%20DOM/Collada%20DOM%202.4/collada-dom-2.4.0.tgz"
+	URL_HASH "MD5=20399de4206eab850c32e65c04e907cb"
+	)
+
+set( FreeGLUT_DOWNLOAD
+	URL "http://downloads.sourceforge.net/project/freeglut/freeglut/3.0.0%20Release%20Candidate%201/freeglut-3.0.0-rc1.tar.gz"
+	URL_HASH "MD5=9679f19e5d9fd211c904698be0cfec2b"
+	)
+	
+set( GLEW_DOWNLOAD
+	URL "http://downloads.sourceforge.net/project/glew/glew/1.11.0/glew-1.11.0-win32.zip"
+	URL_HASH "MD5=ebcebc61bfa1879ece139b7acc4b2734"
+	)
+
+set( inVRs_DOWNLOAD
+	SVN_REPOSITORY "https://svn.lrz.de/repos/inVRs/branches/inVRs_OSG"
+	)
+
+set( JPEG_DOWNLOAD
+	URL "http://downloads.sourceforge.net/project/libjpeg/libjpeg/6b/jpegsr6.zip"
+	URL_HASH "MD5=687a2a8413680732133c5413fc69902b"
+	)
+
+set( LibXML2_DOWNLOAD
+	URL "ftp://xmlsoft.org/libxml2/libxml2-2.9.2.tar.gz"
+	URL_HASH "MD5=9e6a9aca9d155737868b3dc5fd82f788"
 	)
 
 set( OpenSG_DOWNLOAD
 	GIT_REPOSITORY "git://git.code.sf.net/p/opensg/code"
 	)
 
-set( inVRs_DOWNLOAD
-	SVN_REPOSITORY "https://svn.lrz.de/repos/inVRs/branches/inVRs_OSG"
+set( PCRE_DOWNLOAD
+	URL "http://downloads.sourceforge.net/project/pcre/pcre/8.36/pcre-8.36.zip"
+	URL_HASH "MD5=07c8c7cf8f23c41476594f160403dded"
+	)
+
+set( PNG_DOWNLOAD
+	URL "http://download.sourceforge.net/libpng/libpng-1.6.15.tar.gz"
+	URL_HASH "MD5=829a256f3de9307731d4f52dc071916d"
+	)
+
+set( ZLIB_DOWNLOAD
+	URL "http://zlib.net/zlib-1.2.8.tar.gz"
+	URL_HASH "MD5=44d667c142d7cda120332623eab69f40"
 	)
